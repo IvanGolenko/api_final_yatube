58170c1e54b33a013682839cc90c1e6d36e79b60
Иван Голенко <igolivanenko1996@gmail.com>
2022-04-17 18:54:29 +0300

    Спринт 9. Попытка раз

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,11 @@
+from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-
-
-from posts.models import Comment, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
 
     class Meta:
         fields = '__all__'
@@ -21,3 +20,40 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('id', 'title')
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='Вы уже подписаны'
+            )
+        ]
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя'
+            )
+        return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,25 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+
+router.register('posts', PostViewSet, basename='posts')
+router.register('follow', FollowViewSet, basename='follow')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+    basename='comments'
+)
+router.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/auth/', include('djoser.urls')),
+    path('v1/auth/', include('djoser.urls.jwt')),
+    path('v1/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
